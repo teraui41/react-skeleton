74db9ad1c980551b5e2c7bef15481ab978d1839f
Edmond Wu <edmond.wu@velocitylight.net>
2019-07-22 19:49:16 +0800

    InitProject

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-family: "Quicksand", sans-serif;
+  background-color: #181b44;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Container;
---
+++ src/store/configureStore.js
@@ -0,0 +1,13 @@
+import rootReducer from '../reducers';
+import {createStore, compose} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? composeWithDevTools : compose;
+
+const configureStore = () => {
+  const middlewares =[];
+
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
+}
+
+export default configureStore;
